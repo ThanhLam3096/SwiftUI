bfb0353de7e9e535a0929e9e7d5ae8857f1c8ff3
Thanh Lâm <lethanhlam3096@gmail.com>
2025-03-30 17:35:19 +0700

    set up

---
+++ SwiftUISerier/SwiftUISerier/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUISerier
+//
+//  Created by Thanh Lâm on 30/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUISerier/SwiftUISerier/SwiftUISerierApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISerierApp.swift
+//  SwiftUISerier
+//
+//  Created by Thanh Lâm on 30/3/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISerierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
